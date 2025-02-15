c65b97cfebbe17d64776796423b54a7ad29d7869
scottwang <hanqing.a350@gmail.com>
2025-02-15 13:39:02 -0800

    Push

--- app.py
+++ app.py
@@ -18,11 +18,21 @@ def create_app():
     from routes.post import post_bp
     from routes.comment import comment_bp
     from routes.like import like_bp
+    from routes.tag import tag_bp # assuming you will create tag_bp in routes/tag.py
+    from routes.chat import chat_bp # assuming you will create chat_bp in routes/chat.py
+    from routes.user import user_bp # assuming you will create user_bp in routes/user.py
+    from routes.task import task_bp # assuming you will create task_bp in routes/task.py
+
 
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(post_bp, url_prefix='/posts')
     app.register_blueprint(comment_bp, url_prefix='/comments')
     app.register_blueprint(like_bp, url_prefix='/likes')
+    app.register_blueprint(tag_bp, url_prefix='/tags')
+    app.register_blueprint(chat_bp, url_prefix='/chat')
+    app.register_blueprint(user_bp, url_prefix='/users')
+    app.register_blueprint(task_bp, url_prefix='/tasks')
+
 
     return app
 
